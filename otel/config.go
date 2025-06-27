8a370166212bef701a2efa3b42325fc62d9ee6d9
wfhtqp@gmail.com <wfhtqp@gmail.com>
2025-06-27 15:44:33 +0800

    feat: 优化otel日志

---
+++ otel/config.go
@@ -0,0 +1,189 @@
+package otel
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"go.opentelemetry.io/contrib/bridges/otelslog"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/log/global"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+type Config struct {
+	Name          string `json"name" yaml:"name" mapstructure:"name"`
+	LogSource     bool   `json:"logSource" yaml:"logSource" mapstructure:"logSource"`
+	Type          string `json:"type" yaml:"type" mapstructure:"type"`
+	MetricDisable bool   `json:"metricDisable" yaml:"metricDisable" mapstructure:"metricDisable"`
+	TraceDisable  bool   `json:"traceDisable" yaml:"traceDisable" mapstructure:"traceDisable"`
+}
+
+// setupOTelSDK bootstraps the OpenTelemetry pipeline.
+// If it does not return an error, make sure to call shutdown for proper cleanup.
+func (cfg *Config) SetupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	name := "default"
+	if cfg.Name != "" {
+		name = cfg.Name
+	}
+
+	Tracer = otel.Tracer(name)
+	Start = Tracer.Start
+	Meter = otel.Meter(name)
+
+	_log = otelslog.NewLogger(name, otelslog.WithSource(cfg.LogSource))
+
+	Info = _log.InfoContext
+	Error = _log.ErrorContext
+
+	// shutdown calls cleanup functions registered via shutdownFuncs.
+	// The errors from the calls are joined.
+	// Each registered cleanup will be invoked once.
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	if !cfg.TraceDisable {
+		// Set up propagator.
+		prop := newPropagator(cfg)
+		otel.SetTextMapPropagator(prop)
+
+		// Set up trace provider.
+		tracerProvider, nerr := newTraceProvider(cfg)
+		if nerr != nil {
+			handleErr(nerr)
+			return
+		}
+		shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+		otel.SetTracerProvider(tracerProvider)
+	}
+
+	if !cfg.TraceDisable {
+		// Set up meter provider.
+		meterProvider, nerr := newMeterProvider(cfg)
+		if nerr != nil {
+			handleErr(nerr)
+			return
+		}
+		shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+		otel.SetMeterProvider(meterProvider)
+	}
+
+	// Set up logger provider.
+	loggerProvider, err := newLoggerProvider(cfg)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
+	global.SetLoggerProvider(loggerProvider)
+
+	return
+}
+
+func newPropagator(cfg *Config) propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
+
+func newTraceProvider(cfg *Config) (*trace.TracerProvider, error) {
+	var exporter trace.SpanExporter
+	var err error
+
+	switch cfg.Type {
+	case "http":
+		exporter, err = otlptracehttp.New(
+			context.Background(),
+		)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		exporter, err = stdouttrace.New()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	traceProvider := trace.NewTracerProvider(
+		trace.WithSampler(trace.AlwaysSample()),
+		trace.WithBatcher(exporter,
+			// Default is 5s. Set to 1s for demonstrative purposes.
+			trace.WithBatchTimeout(time.Second)),
+	)
+	return traceProvider, nil
+}
+
+func newMeterProvider(cfg *Config) (*metric.MeterProvider, error) {
+	var exporter metric.Exporter
+	var err error
+
+	switch cfg.Type {
+	case "http":
+		exporter, err = otlpmetrichttp.New(
+			context.Background(),
+		)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		exporter, err = stdoutmetric.New()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithReader(metric.NewPeriodicReader(exporter, metric.WithInterval(3*time.Second))),
+		// Default is 1m. Set to 3s for demonstrative purposes.
+	)
+	return meterProvider, nil
+}
+
+func newLoggerProvider(cfg *Config) (*log.LoggerProvider, error) {
+	var exporter log.Exporter
+	var err error
+
+	switch cfg.Type {
+	case "http":
+		exporter, err = otlploghttp.New(
+			context.Background(),
+		)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		exporter, err = stdoutlog.New()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	loggerProvider := log.NewLoggerProvider(
+		log.WithProcessor(log.NewBatchProcessor(exporter)),
+	)
+	return loggerProvider, nil
+}
--- otel/otel.go
+++ otel/otel.go
@@ -2,171 +2,42 @@ package otel
 
 import (
 	"context"
-	"errors"
-	"time"
+	"log/slog"
 
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
-	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
-	"go.opentelemetry.io/otel/log/global"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/log"
-	"go.opentelemetry.io/otel/sdk/metric"
-	"go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
 )
 
-type Config struct {
-	Type          string `json:"type" yaml:"type" mapstructure:"type"`
-	MetricDisable bool   `json:"metricDisable" yaml:"metricDisable" mapstructure:"metricDisable"`
-	TraceDisable  bool   `json:"traceDisable" yaml:"traceDisable" mapstructure:"traceDisable"`
+var (
+	_log  *slog.Logger
+	Info  func(ctx context.Context, msg string, args ...any)
+	Error func(ctx context.Context, msg string, args ...any)
+)
+
+var (
+	Tracer trace.Tracer
+	Start  func(ctx context.Context, spanName string, opts ...trace.SpanStartOption) (context.Context, trace.Span)
+)
+
+var Meter metric.Meter
+
+type ctxLogKey struct{}
+
+var _ctxLogKey = &ctxLogKey{}
+
+func ForLog(ctx context.Context, args ...any) (*slog.Logger, context.Context) {
+	contextLog := ctx.Value(_ctxLogKey)
+	if contextLog != nil {
+		if len(args) == 0 {
+			return contextLog.(*slog.Logger), ctx
+		}
+		l := contextLog.(*slog.Logger).With(args...)
+		return l, CtxLog(ctx, l)
+	}
+	l := _log.With(args...)
+	return l, CtxLog(ctx, l)
 }
 
-// setupOTelSDK bootstraps the OpenTelemetry pipeline.
-// If it does not return an error, make sure to call shutdown for proper cleanup.
-func (cfg *Config) SetupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, err error) {
-	var shutdownFuncs []func(context.Context) error
-
-	// shutdown calls cleanup functions registered via shutdownFuncs.
-	// The errors from the calls are joined.
-	// Each registered cleanup will be invoked once.
-	shutdown = func(ctx context.Context) error {
-		var err error
-		for _, fn := range shutdownFuncs {
-			err = errors.Join(err, fn(ctx))
-		}
-		shutdownFuncs = nil
-		return err
-	}
-
-	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
-	handleErr := func(inErr error) {
-		err = errors.Join(inErr, shutdown(ctx))
-	}
-
-	if !cfg.TraceDisable {
-		// Set up propagator.
-		prop := newPropagator(cfg)
-		otel.SetTextMapPropagator(prop)
-
-		// Set up trace provider.
-		tracerProvider, nerr := newTraceProvider(cfg)
-		if nerr != nil {
-			handleErr(nerr)
-			return
-		}
-		shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
-		otel.SetTracerProvider(tracerProvider)
-	}
-
-	if !cfg.TraceDisable {
-		// Set up meter provider.
-		meterProvider, nerr := newMeterProvider(cfg)
-		if nerr != nil {
-			handleErr(nerr)
-			return
-		}
-		shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
-		otel.SetMeterProvider(meterProvider)
-	}
-
-	// Set up logger provider.
-	loggerProvider, err := newLoggerProvider(cfg)
-	if err != nil {
-		handleErr(err)
-		return
-	}
-	shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
-	global.SetLoggerProvider(loggerProvider)
-
-	return
-}
-
-func newPropagator(cfg *Config) propagation.TextMapPropagator {
-	return propagation.NewCompositeTextMapPropagator(
-		propagation.TraceContext{},
-		propagation.Baggage{},
-	)
-}
-
-func newTraceProvider(cfg *Config) (*trace.TracerProvider, error) {
-	var exporter trace.SpanExporter
-	var err error
-
-	switch cfg.Type {
-	case "http":
-		exporter, err = otlptracehttp.New(
-			context.Background(),
-		)
-		if err != nil {
-			return nil, err
-		}
-	default:
-		exporter, err = stdouttrace.New()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	traceProvider := trace.NewTracerProvider(
-		trace.WithSampler(trace.AlwaysSample()),
-		trace.WithBatcher(exporter,
-			// Default is 5s. Set to 1s for demonstrative purposes.
-			trace.WithBatchTimeout(time.Second)),
-	)
-	return traceProvider, nil
-}
-
-func newMeterProvider(cfg *Config) (*metric.MeterProvider, error) {
-	var exporter metric.Exporter
-	var err error
-
-	switch cfg.Type {
-	case "http":
-		exporter, err = otlpmetrichttp.New(
-			context.Background(),
-		)
-		if err != nil {
-			return nil, err
-		}
-	default:
-		exporter, err = stdoutmetric.New()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	meterProvider := metric.NewMeterProvider(
-		metric.WithReader(metric.NewPeriodicReader(exporter, metric.WithInterval(3*time.Second))),
-		// Default is 1m. Set to 3s for demonstrative purposes.
-	)
-	return meterProvider, nil
-}
-
-func newLoggerProvider(cfg *Config) (*log.LoggerProvider, error) {
-	var exporter log.Exporter
-	var err error
-
-	switch cfg.Type {
-	case "http":
-		exporter, err = otlploghttp.New(
-			context.Background(),
-		)
-		if err != nil {
-			return nil, err
-		}
-	default:
-		exporter, err = stdoutlog.New()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	loggerProvider := log.NewLoggerProvider(
-		log.WithProcessor(log.NewBatchProcessor(exporter)),
-	)
-	return loggerProvider, nil
+func CtxLog(ctx context.Context, log *slog.Logger) context.Context {
+	return context.WithValue(ctx, _ctxLogKey, log)
 }
